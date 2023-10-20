b803102b6a1e1a24d13c5e83e93ed061af04b4ac
Judiao Mbaua <jumba2010@gmail.com>
2023-10-20 12:37:14 +0200

    Changing backend testing endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { getToken } from './auth';
 
-export const baseURL = 'https://bz2d6disx4.execute-api.us-east-1.amazonaws.com/prod/';
+export const baseURL = 'https://608xjr0gcg.execute-api.us-east-1.amazonaws.com/test/';
 
 const api = axios.create({
-  baseURL: 'https://bz2d6disx4.execute-api.us-east-1.amazonaws.com/prod',
+  baseURL: 'https://608xjr0gcg.execute-api.us-east-1.amazonaws.com/test/',
   rejectUnauthorized: false
 });
 
