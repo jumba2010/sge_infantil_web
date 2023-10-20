77ffe53282045c02ec5c88b416f7298250f21161
Judiao Mbaua <jumba2010@gmail.com>
2023-10-20 13:31:03 +0200

    Changing backend testing endpoint

--- src/pages/account/center/service.ts
+++ src/pages/account/center/service.ts
@@ -1,7 +1,7 @@
 import request from '@/utils/request';
 
 export async function queryCurrent() {
-  return request('/api/currentUser');
+  return {name:"Utilizador de Testes"};
 }
 
 export async function queryFakeList(params: { count: number }) {
